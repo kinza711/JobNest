1fee539a3a8a171bdae6e4ea989499acde02de77
kinza <kinzashahzadi7181@gmail.com>
2026-03-09 04:23:10 +0500

    fix: jwt varifytoken error fixed

--- backend/src/Middlewares/varifyToken.js
+++ backend/src/Middlewares/varifyToken.js
@@ -1,35 +1,28 @@
-import Users from "../Models/authModel.js";
 import jwt from "jsonwebtoken";
-const varifyToken = async (req, res, next) => {
+import Users from "../Models/authModel.js";
+
+const verifyToken = async (req, res, next) => {
   try {
     // step 1 to set authheader
     const authHeader = req.headers.authorization;
-    // to check authheader
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-      return res.status(401).json({
-        message: "token not found",
-      });
-    }
-    // varify token with sectet key
-    const token = authHeader.split("")[1];
-    const decoded = jwt.varify(token, process.env.JWT_SECRET);
 
-    // this is the key ( Mian ) part to find users
-    const user = await Users.findById(decoded.id).select("-password");
-    if (!user) {
-      return res.status(401).json({
-        message: " user not varified",
-      });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "User not logged in" });
     }
 
-    req.user = user; // impprtant part
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await Users.findById(decoded.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user; // attach user to request
     next();
   } catch (err) {
-    console.error(err);
-    res.status(401).json({
-      message: "token or user not varified",
-    });
+    res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
-export default varifyToken;
+export default verifyToken;
