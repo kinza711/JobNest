ff64c3b551d2d67922dfdf22f7721b03d9a86594
kinza <kinzashahzadi7181@gmail.com>
2026-03-28 04:00:32 +0500

    feat: email services updated

--- backend/src/Config/emailconfig.js
+++ backend/src/Config/emailconfig.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Transporter = nodemailer.createTransport({
-  service: "gmail",
+  //service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
