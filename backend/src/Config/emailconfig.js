23f74af5547f47064d36c4e138b85abd7c30a3f8
kinza <kinzashahzadi7181@gmail.com>
2026-03-28 05:40:27 +0500

    port upadred for SMTP mail

--- backend/src/Config/emailconfig.js
+++ backend/src/Config/emailconfig.js
@@ -5,8 +5,9 @@ dotenv.config();
 const Transporter = nodemailer.createTransport({
   //service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  //port: 465,
+  port: 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
